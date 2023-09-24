5ba59f6d7fd276af5f839f82e2f18b55517263f0
Diogo Lima Carvalho <diogo.carvalho.lc@gmail.com>
2023-09-24 14:20:13 -0300

    Finaliza tela 'Lista de desejos'
    - Finaliza estrutura de card da página
    
    - Finaliza responsividade da página
    
    - Arruma estilo do componente <HeaderWithFilter>

--- src/pages/ListadeDesejos/ListadeDesejos.jsx
+++ src/pages/ListadeDesejos/ListadeDesejos.jsx
@@ -23,7 +23,7 @@ const ListadeDesejos = () => {
 
               <div className='listaDeDesejos__card__info'>
                 <div className='listaDeDesejos__card__info__title'>
-                  <p>Celeste</p>
+                  <h2>Celeste</h2>
                   <p>R$ 8,00</p>
                 </div>
 
@@ -37,7 +37,36 @@ const ListadeDesejos = () => {
               </div>
             </div>
 
-            <div>
+            <div className='listaDeDesejos__trash'>
+              <img src="../../../public/imgs/icons/trash_icon.svg" alt="ícone de carrinho" />
+            </div>
+          </div>
+
+
+
+          <div className='listaDeDesejos__cards__contain'>
+            <div className='listaDeDesejos__card'>
+              <div className='listaDeDesejos__card__image'>
+                <img src="../../../public/imgs/jogos/meusjogos_02.png" alt="imagem do jogo" />
+              </div>
+
+              <div className='listaDeDesejos__card__info'>
+                <div className='listaDeDesejos__card__info__title'>
+                  <h2>A lenda do herói</h2>
+                  <p>R$ 15,00</p>
+                </div>
+
+                <div className='listaDeDesejos__card__info__actions'>
+                  <div>
+                    <img src="../../../public/imgs/icons/detalhes__icon.svg" alt="ícone de olho representando mais detalhes do jogo" />
+                    <p>Detalhes</p>
+                  </div>
+                  <img src="../../../public/imgs/icons/cart__icon.svg" alt="ícone de carrinho" />
+                </div>
+              </div>
+            </div>
+
+            <div className='listaDeDesejos__trash'>
               <img src="../../../public/imgs/icons/trash_icon.svg" alt="ícone de carrinho" />
             </div>
           </div>
