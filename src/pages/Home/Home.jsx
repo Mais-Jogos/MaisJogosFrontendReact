76188e6d69a79862a2d692ccbb4ad8f1b169b9d0
Aryel Anne <aryel.anne@sonne.global>
2023-09-29 12:30:09 -0300

    Filtro Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -31,11 +31,16 @@ const Home = () => {
   const generos = [].concat(...games.map((game) => game.genres))
   const plataformas = [].concat(...games.map((game) => game.parent_platforms))
   const plataformas2 = [].concat(...plataformas.map((game) => game.platform))
-  const changeFilter = (filterName, value) =>{
+  const changeFilter = (e, filterName, value) =>{
+
     if(filter[filterName] !== 'Todos'){
-      if(filter[filterName].some(filter => filter === filterName)){
-      const updtFilter = filter[filterName].filter((valueFilter) => valueFilter !== value)
-      setFilter({...filter, [filterName]: updtFilter});
+      if(!e.target.checked){
+        const updtFilter = filter[filterName].filter((valueFilter) => valueFilter !== value)
+        if(!updtFilter){
+          setFilter({...filter, [filterName]: updtFilter});
+        }else{
+          setFilter({...filter, [filterName]: "Todos"});
+        }
       }else{
         setFilter({...filter, [filterName]: [...filter[filterName], value]})
       }
@@ -43,6 +48,26 @@ const Home = () => {
       setFilter({...filter, [filterName]: [value]})
     }
   }
+
+  useEffect(()=>{
+    const jogosFiltrados = games?.filter(jogo => {
+      var plataformasSelecionadas;
+      var categoriasSelecionadas;
+      if(filter.platform !== "Todos"){
+        plataformasSelecionadas = jogo.platforms.some(platforma => filter.platform.includes(platforma.platform.name));
+      }else{
+        plataformasSelecionadas = jogo;
+      }
+      if(filter.category !== "Todos"){
+        categoriasSelecionadas = jogo.genres.some(categoria => filter.category.includes(categoria.name));
+      }else{
+        categoriasSelecionadas = jogo;
+      }
+      return plataformasSelecionadas && categoriasSelecionadas;
+
+    })
+    console.log(jogosFiltrados);
+  },[filter])
   return (
     <div id='container-page' className='home'>
       <Menu/>
@@ -58,8 +83,8 @@ const Home = () => {
             {
               [...new Set(generos?.map((game) => game.name))].map(category => (
                 <div className='home__categorias'>
-                  <input type="checkbox" name="Categorias" id={category} />
-                  <label key={category} htmlFor={category} onClick={() => changeFilter('category', category)}>{category}</label>
+                  <input type="checkbox" name="Categorias" id={category} onClick={(e) => changeFilter(e, 'category', category)}/>
+                  <label key={category} htmlFor={category} >{category}</label>
                 </div>
               ))
             }
@@ -67,8 +92,8 @@ const Home = () => {
             {              
               [...new Set(plataformas2?.map((game) => game.name))].map(platform => (
                 <div className='home__categorias'>
-                  <input type="checkbox" name="Plataformas" id={platform} />
-                  <label key={platform} htmlFor={platform} onClick={() => changeFilter('platform', platform)}>{platform}</label>
+                  <input type="checkbox" name="Plataformas" id={platform} onClick={(e) => changeFilter(e, 'platform', platform)}/>
+                  <label key={platform} htmlFor={platform} >{platform}</label>
                 </div>
               ))
             }
@@ -76,7 +101,7 @@ const Home = () => {
         <div className="container__home__right">
           <div className="section__title">
             <h1>
-              Loja nacional de jogos indie
+              Loja nacional de jogos indieLinod
             </h1>
 {/*             <img src={`../../public/imgs/animais/${image}.png`} alt="" />
  */}          </div>
@@ -92,11 +117,22 @@ const Home = () => {
           <div id="games__home">
             <h2>Novidades</h2>
             <div className="section__games">
-              {games?.filter((
-                  game =>
-                    (filter.platform === 'Todos' || game.parent_platforms.some(genre => genre.platform.name === filter.platform)) &&
-                    (filter.category === 'Todos' || game.genres.some(genre => genre.name === filter.category))
-                )).slice(0,numberGames).map((game, index)=>(
+              {games?.filter(jogo => {
+                var plataformasSelecionadas;
+                var categoriasSelecionadas;
+                if(filter.platform !== "Todos"){
+                  plataformasSelecionadas = jogo.platforms.some(platforma => filter.platform.includes(platforma.platform.name));
+                }else{
+                  plataformasSelecionadas = jogo;
+                }
+                if(filter.category !== "Todos"){
+                  categoriasSelecionadas = jogo.genres.some(categoria => filter.category.includes(categoria.name));
+                }else{
+                  categoriasSelecionadas = jogo;
+                }
+                return plataformasSelecionadas && categoriasSelecionadas;
+
+              }).slice(0,numberGames).map((game, index)=>(
                 <Card games={games} game={index}/>
               ))}
             </div>
