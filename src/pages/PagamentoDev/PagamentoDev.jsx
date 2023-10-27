f670f66fca3d12366e5ecb43a8ba397d57e2ab0a
DiogoLCarvalho <diogo.carvalho.lc@gmail.com>
2023-10-27 09:38:45 -0300

    :construction: Pagamento Dev
    Arruma sequência lógica da página

--- src/pages/PagamentoDev/PagamentoDev.jsx
+++ src/pages/PagamentoDev/PagamentoDev.jsx
@@ -5,39 +5,57 @@ import Acessibilidade from '../../components/Acessibilidade/Acessibilidade'
 import Footer from "../../components/Footer/Footer";
 import Vlibras from '../../components/Vlibras/Vlibras'
 import { useState } from "react";
-
+import InputsPagamentoDev from "./InputsPagamentoDev";
+import { Link } from "react-router-dom";
 
 function reducerTypeOfPix(state, action) {
     switch (action.type) {
         case "cpf":
-            return { cpf: action.cpf, telefone: false, email: false, aleatorio: false }
+            return { cpf: { status: action.cpf, value: state.cpf.value }, telefone: { status: false, value: state.telefone.value }, email: { status: false, value: state.email.value }, aleatorio: { status: false, value: state.aleatorio.value } }
+        case "cpf_text":
+            return { cpf: { status: state.cpf.status, value: action.cpf }, telefone: { status: state.telefone.status, value: state.telefone.value }, email: { status: state.email.status, value: state.email.value }, aleatorio: { status: state.aleatorio.status, value: state.aleatorio.value } }
         case "telefone":
-            return { cpf: false, telefone: action.telefone, email: false, aleatorio: false }
+            return { cpf: { status: false, value: state.cpf.value }, telefone: { status: action.telefone, value: state.telefone.value }, email: { status: false, value: state.email.value }, aleatorio: { status: false, value: state.aleatorio.value } }
+        case "telefone_text":
+            return { cpf: { status: state.cpf.status, value: state.cpf.value }, telefone: { status: state.telefone.status, value: action.telefone }, email: { status: state.email.status, value: state.email.value }, aleatorio: { status: state.aleatorio.status, value: state.aleatorio.value } }
         case "email":
-            return { cpf: false, telefone: false, email: action.email, aleatorio: false }
+            return { cpf: { status: false, value: state.cpf.value }, telefone: { status: false, value: state.telefone.value }, email: { status: action.email, value: state.email.value }, aleatorio: { status: false, value: state.aleatorio.value } }
+        case "email_text":
+            return { cpf: { status: state.cpf.status, value: state.cpf.value }, telefone: { status: state.telefone.status, value: state.telefone.value }, email: { status: state.email.status, value: action.email }, aleatorio: { status: state.aleatorio.status, value: state.aleatorio.value } }
         case "aleatorio":
-            return { cpf: false, telefone: false, email: false, aleatorio: action.aleatorio }
+            return { cpf: { status: false, value: state.cpf.value }, telefone: { status: false, value: state.telefone.value }, email: { status: false, value: state.email.value }, aleatorio: { status: action.aleatorio, value: state.aleatorio.value } }
+        case "aleatorio_text":
+            return { cpf: { status: state.cpf.status, value: state.cpf.value }, telefone: { status: state.telefone.status, value: state.telefone.value }, email: { status: state.email.status, value: state.email.value }, aleatorio: { status: state.aleatorio.status, value: action.aleatorio } }
         default:
             return state;
     }
 }
 
 
+function setSendDataButtonsBack(state, setState) {
+    if (state == 0) {
+        setState(0)
+    } else {
+        setState(e => e - 1)
+    }
+}
+
+function setSendDataButtonsGo(state, setState) {
+    console.log(state);
+    setState(e => e + 1)
+}
+
 export default props => {
 
     const [typeOfPix, dispatch] = useReducer(reducerTypeOfPix, {
-        cpf: true,
-        telefone: false,
-        email: false,
-        aleatorio: false
+        cpf: { status: true, value: "" },
+        telefone: { status: false, value: "" },
+        email: { status: false, value: "" },
+        aleatorio: { status: false, value: "" }
     });
 
-    const [storeCpf, setStoreCpf] = useState("");
-    const [storeTelefone, setStoreTelefone] = useState("");
-    const [storeEmail, setStoreEmail] = useState("");
-    const [storeAleatorio, setStoreAleatorio] = useState("");
-    const [storeNomeCompleto, setStoreNomeCompleto] = useState("");
-    const [enviar, setEnviar] = useState(true);
+    const [enviar, setEnviar] = useState(0);
+    const [nomePix, setNomePix] = useState("");
     const [selectTypePix, setselectTypePix] = useState(true);
 
 
@@ -72,51 +90,51 @@ export default props => {
 
                     <div className="pagamentoDev__pixContainer__content">
 
-                        { selectTypePix ? (<>
-                            <h2>{enviar ? "Selecione o tipo do seu pix" : "Confirmar pix"}</h2>
+                        {selectTypePix ? (<>
+                            <h2>{enviar == 0 ? "Selecione o tipo do seu pix" : "Confirmar pix"}</h2>
 
-                            {enviar ? (<div className="pagamentoDev__pixContainer__content__methods">
+                            {enviar == 0 ? (<div className="pagamentoDev__pixContainer__content__methods">
                                 <div className="pagamentoDev__pixContainer__content__methods__container">
-                                    <button className={typeOfPix.cpf ? "pagamentoDev__pixContainer__content__methods__container--select" : ""} onClick={e => { dispatch({ type: 'cpf', cpf: true }) }}>CPF</button>
+                                    <button className={typeOfPix.cpf.status ? "pagamentoDev__pixContainer__content__methods__container--select" : ""} onClick={e => { dispatch({ type: 'cpf', cpf: true }); }}>CPF</button>
                                 </div>
                                 <div className="pagamentoDev__pixContainer__content__methods__container">
-                                    <button className={typeOfPix.telefone ? "pagamentoDev__pixContainer__content__methods__container--select" : ""} onClick={e => { dispatch({ type: 'telefone', telefone: true }) }}>Telefone</button>
+                                    <button className={typeOfPix.telefone.status ? "pagamentoDev__pixContainer__content__methods__container--select" : ""} onClick={e => { dispatch({ type: 'telefone', telefone: true }) }}>Telefone</button>
                                 </div>
                                 <div className="pagamentoDev__pixContainer__content__methods__container">
-                                    <button className={typeOfPix.email ? "pagamentoDev__pixContainer__content__methods__container--select" : ""} onClick={e => { dispatch({ type: 'email', email: true }) }}>Email</button>
+                                    <button className={typeOfPix.email.status ? "pagamentoDev__pixContainer__content__methods__container--select" : ""} onClick={e => { dispatch({ type: 'email', email: true }) }}>Email</button>
                                 </div>
                                 <div className="pagamentoDev__pixContainer__content__methods__container">
-                                    <button className={typeOfPix.aleatorio ? "pagamentoDev__pixContainer__content__methods__container--select" : ""} onClick={e => { dispatch({ type: 'aleatorio', aleatorio: true }) }}>Aleatório</button>
+                                    <button className={typeOfPix.aleatorio.status ? "pagamentoDev__pixContainer__content__methods__container--select" : ""} onClick={e => { dispatch({ type: 'aleatorio', aleatorio: true }) }}>Aleatório</button>
                                 </div>
                             </div>) : ""}
 
-                            <div className="pagamentoDev__pixContainer__content__register">
+                            {/* <div className="pagamentoDev__pixContainer__content__register">
                                 <div>
-                                    {typeOfPix.cpf ? (
+                                    {typeOfPix.cpf.status ? (
                                         <>
                                             <label htmlFor="cpf">CPF</label>
-                                            <input type="text" name="cpf" id="cpf" value={storeCpf} onChange={e => { setStoreCpf(e.target.value) }} />
+                                            <input type="text" name="cpf" id="cpf" value={typeOfPix.cpf.value} onChange={e => {dispatch({ type: 'cpf_text', cpf: e.target.value }) }} />
                                         </>
                                     ) : ""}
 
-                                    {typeOfPix.telefone ? (
+                                    {typeOfPix.telefone.status ? (
                                         <>
                                             <label htmlFor="telefone">Telefone</label>
-                                            <input type="tel" name="telefone" id="telefone" value={storeTelefone} onChange={e => { setStoreTelefone(e.target.value) }} />
+                                            <input type="tel" name="telefone" id="telefone" value={typeOfPix.telefone.value} onChange={e => { dispatch({ type: 'telefone_text', telefone: e.target.value }) }} />
                                         </>
                                     ) : ""}
 
-                                    {typeOfPix.email ? (
+                                    {typeOfPix.email.status ? (
                                         <>
                                             <label htmlFor="email">Email</label>
-                                            <input type="email" name="email" id="email" value={storeEmail} onChange={e => { setStoreEmail(e.target.value) }} />
+                                            <input type="email" name="email" id="email" value={typeOfPix.email.value} onChange={e => { dispatch({ type: 'email_text', email: e.target.value }) }} />
                                         </>
                                     ) : ""}
 
-                                    {typeOfPix.aleatorio ? (
+                                    {typeOfPix.aleatorio.status ? (
                                         <>
                                             <label htmlFor="aleatorio">Aleatório</label>
-                                            <input type="text" name="aleatorio" id="aleatorio" value={storeAleatorio} onChange={e => { setStoreAleatorio(e.target.value) }} />
+                                            <input type="text" name="aleatorio" id="aleatorio" value={typeOfPix.aleatorio.value} onChange={e => { dispatch({ type: 'aleatorio_text', aleatorio: e.target.value }) }} />
                                         </>
                                     ) : ""}
 
@@ -124,26 +142,29 @@ export default props => {
 
                                 <div>
                                     <label htmlFor="nome">Nome completo</label>
-                                    <input type="text" name="nome" id="nome" value={storeNomeCompleto} onChange={e => { setStoreNomeCompleto(e.target.value) }} />
+                                    <input type="text" name="nome" id="nome" value={nomePix} onChange={e => { setNomePix(e.target.value) }} />
                                 </div>
-                            </div>
+                            </div> */}
+
+                            <InputsPagamentoDev typeOfPix={typeOfPix} nomePix={nomePix} setNomePix={setNomePix} dispatch={dispatch} />
+
                         </>)
 
-                        : 
+                            :
 
-                        (<>
-                            <h2>Selecione o pix</h2>
+                            (<>
+                                <h2>Selecione o pix</h2>
 
-                            <div className="pagamentoDev__pixContainer__content__methods">
-                                <button className="pagamentoDev__pixContainer__content__methods__buttonPixSalvo">Ana Maria</button>
-                                <button className="pagamentoDev__pixContainer__content__methods__buttonPixSalvo">Jorge Raimundo</button>
-                                <button className="pagamentoDev__pixContainer__content__methods__buttonPixSalvo">Wesley Araujo</button>
-                            </div>
-                        </>)}
+                                <div className="pagamentoDev__pixContainer__content__methods">
+                                    <button className="pagamentoDev__pixContainer__content__methods__buttonPixSalvo">Ana Maria</button>
+                                    <button className="pagamentoDev__pixContainer__content__methods__buttonPixSalvo">Jorge Raimundo</button>
+                                    <button className="pagamentoDev__pixContainer__content__methods__buttonPixSalvo">Wesley Araujo</button>
+                                </div>
+                            </>)}
 
                         <div className="pagamentoDev__pixContainer__content__methods__buttons">
-                            <button>Cancelar</button>
-                            <button>Confirmar</button>
+                            <button onClick={_ => {enviar == 0 ? "" : setEnviar(e => e - 1 )}}>Cancelar</button>
+                            <button onClick={_ => setEnviar(e => e + 1)}>{ enviar == 0 ? "Confirmar" : "Enviar"}</button>
                         </div>
                     </div>
                 </section>
@@ -157,13 +178,15 @@ export default props => {
                     </div>
                 </section>
 
-                <div className="pagamentoDev__modalConfirm">
+                <div className="pagamentoDev__modalBackground"  style={enviar == 2 ? { display: "flex"} : {display: "none"}}>
+                    <div className="pagamentoDev__modalConfirm">
                         <div className="pagamentoDev__modalConfirm__container">
                             <div>
                                 <p>Seus dados foram enviados com sucesso, prazo de até 3 (três) dias úteis para o valor cair na sua conta. Após 15 dias do pagamento realizado você poderá requerir um novo pagamento</p>
                             </div>
-                            <div><button>OK</button></div>
+                            <div><Link to="/perfil-dev">OK</Link></div>
                         </div>
+                    </div>
                 </div>
             </main>
             <Footer />
